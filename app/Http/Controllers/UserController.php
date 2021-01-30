55d0f0083a342a06a0e0c55e2da864114c9db4b9
yohesky <yoheskyjpp@gmail.com>
2021-01-30 10:41:11 -0400

    first commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\request\User as RequestUser;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $users = User::all();
+        return response()->json($users);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create($request)
+    {
+        //
+        return "hola";
+        // User::create($request);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+       $user = User::create($request->all());
+       return response()->json($user,201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+        $user = User::find($id);
+        if(!$user){
+            return response()->json($msg = "error", 404);
+        }
+        return response()->json($user);
+
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+        $user = User::findOrFail($id);
+
+        if(!$user){
+            return response()->json($msg = "user not found");
+        }
+
+        $user->update($request->all());
+
+        return $user;
+
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+        $user = User::find($id)->delete();
+        if(!$user){
+            return response()->json($msg = "user not found");
+        }
+        return response()->json($msg = "user deleted");
+    }
+}
---
+++ app/Http/request/User.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\request;
+
+use Illuminate\Foundation\Http\FormRequest;
+class User extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            //
+            "name" => "required",
+            "lastname" => "required",
+            "email" => "email|required",
+            "phone" => "numeric|required",
+            "addres" => "required"
+        ];
+    }
+}
